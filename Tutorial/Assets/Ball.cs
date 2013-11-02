9cbfbe731e773922c92b605f0cb1a69240a2cbb2
t-hrhs <hirohashi.t.0204@gmail.com>
2013-11-03 01:02:53 +0900

    Tutorialのディレクトリ作成及び、assetの追加

---
+++ Tutorial/Assets/Ball.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.rigidbody.velocity = new Vector3(-8.0f, 8.0f,  0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
