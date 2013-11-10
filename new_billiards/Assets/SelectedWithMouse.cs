216ddb076644c8c03979386a292cfa8405ef0b8e
t-hrhs <hirohashi.t.0204@gmail.com>
2013-11-11 00:30:57 +0900

    適切な物理設定を施す 反射・マウスでの選択判定をできるようにしました。

---
+++ new_billiards/Assets/SelectedWithMouse.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectedWithMouse : MonoBehaviour {
+	public static GameObject selectedGameObject;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetMouseButtonDown(0)){
+			Ray ray;
+			RaycastHit hit = new RaycastHit();
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
+			if (Physics.Raycast(ray,out hit)) {
+				selectedGameObject = hit.collider.gameObject;	
+			} else {
+				selectedGameObject = null;	
+			}
+		}
+	}
+}
