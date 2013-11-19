255f2556b6e2bc8e2de520f0f3c4b8c2dce59286
t-hrhs <hirohashi.t.0204@gmail.com>
2013-11-19 16:17:58 +0900

    holeのcolliderを削除し、GameControllerでholeの座標を保持する事で判定

--- new_billiards/Assets/Script/GameController.cs
+++ new_billiards/Assets/Script/GameController.cs
@@ -13,6 +13,15 @@ public class  GameController : MonoBehaviour {
 	public GameObject[] b = new GameObject[4];
 	public int[] b_hp = {100,200,300,400};
 	public GUIText[] b_hp_text = new GUIText[4];
+	//hole's information
+	public Vector3[] hall_points = {
+		new Vector3(-11.5f,0.5f,5.3f),
+		new Vector3(-11.5f,0.5f,-5.3f),
+		new Vector3(0.0f,0.5f,5.3f),
+		new Vector3(0.0f,0.5f,-5.3f),
+		new Vector3(11.5f,0.5f,5.3f),
+		new Vector3(11.5f,0.5f,-5.3f),
+	};
 	
 	//my original method
 	GameObject make_ball(int x, int z) {
@@ -43,6 +52,21 @@ public class  GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		for (int i=0;i<4;i++){
+			if(is_in_a_hole(a[i])) {
+				a[i].transform.position = new Vector3(0,-5,0);
+			}
+			if(is_in_a_hole(b[i])) {
+				b[i].transform.position = new Vector3(0,-5,0);
+			}
+		}
+	}
+	bool is_in_a_hole (GameObject ball) {
+		for (int i=0;i<6;i++){
+			if (Vector3.Distance(ball.transform.position,hall_points[i]) < 1) {
+				return true;	
+			}
+		}
+		return false;
 	}
 }
