373d911c9ad009d1d45cc449cd5ed1791c99c7be
t-hrhs <hirohashi.t.0204@gmail.com>
2013-11-20 23:20:12 +0900

    ボール射出時のバグ(同時発射を修正)

--- new_billiards/Assets/Script/SelectedWithMouse.cs
+++ new_billiards/Assets/Script/SelectedWithMouse.cs
@@ -17,14 +17,16 @@ public class SelectedWithMouse : MonoBehaviour {
 	}
 	//Get mouse information
 	void get_touched_object_by_mouse(){
-		Ray ray;
-		RaycastHit hit = new RaycastHit();
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		Debug.DrawRay (ray.origin, ray.direction * 10, Color.yellow);
-		if (Physics.Raycast(ray,out hit)) {
-			selectedGameObject = hit.collider.gameObject;	
-		} else {
-			selectedGameObject = null;	
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("left click");
+			Ray ray;
+			RaycastHit hit = new RaycastHit();
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray,out hit)) {
+				selectedGameObject = hit.collider.gameObject;	
+			} else {
+				selectedGameObject = null;	
+			}
 		}
 	}
 }
