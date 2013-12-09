d37ca65a24e8542daef78e786ab1871ce5873f28
t-hrhs <hirohashi.t.0204@gmail.com>
2013-12-10 00:33:47 +0900

    ゲージ表示の方法やスコアの表示を実装 残実装：ターン数,方向,ゲージ操作,ボールとの連動

--- count_up/Assets/Script/Hole.cs
+++ count_up/Assets/Script/Hole.cs
@@ -12,9 +12,16 @@ public class Hole : MonoBehaviour {
 	void Update () {
 	
 	}
-	/*void OnCollisionEnter(Collision collision) {
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log(collision.gameObject.tag);
 		if (collision.gameObject.tag == "Ball") {
 			collision.gameObject.transform.position = new Vector3(0,-5,0);
+			Ball ballscript = collision.gameObject.GetComponent<Ball>();
+			GameController.score +=  (ballscript.color_id + 1) * GameController.turn;
+		} else if (collision.gameObject.tag == "MyBall") {
+			//My Ball goes back to start position
+			collision.gameObject.rigidbody.velocity = new Vector3(0,0,0);
+			collision.gameObject.transform.position = new Vector3((float)-5,(float)0.5f,1);
 		}
-	}*/
+	}
 }
