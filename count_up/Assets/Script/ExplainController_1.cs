5d23b1a6f37c70a7a5c4088b252dc3feef29ab5c
t-hrhs <hirohashi.t.0204@gmail.com>
2013-12-21 17:37:11 +0900

    【count_up】 : ゲーム説明画面を追加 + 画面遷移できるようにする

---
+++ count_up/Assets/Script/ExplainController_1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplainController_1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI () {
+		if(GUI.Button(new Rect(100,250,200,50),"Back to the main !!")) {
+			//Go to the 1st STG
+			Application.LoadLevel("start_scene");
+			Debug.Log("bottun pushed");
+		}
+		if(GUI.Button(new Rect(350,250,200,50), "Go to the stage !!")) {
+			//Go to the 1st STG
+			Application.LoadLevel("main_game_scene_1");
+			Debug.Log("bottun pushed");
+		}
+	}
+}
