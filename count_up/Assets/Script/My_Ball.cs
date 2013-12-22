241629737f152c0914c8ccc6a038059c4c64a7c1
t-hrhs <hirohashi.t.0204@gmail.com>
2013-12-22 14:34:01 +0900

    【count_up】 : 背景画像の変更 + 穴の画像変更

--- count_up/Assets/Script/My_Ball.cs
+++ count_up/Assets/Script/My_Ball.cs
@@ -18,8 +18,8 @@ public class My_Ball : MonoBehaviour {
 		first_velocity.y = 0.0f;
 		if (Input.GetMouseButtonUp(0)) {
 			this.rigidbody.velocity = first_velocity;
-			GameController.user_touchable = false;
-			GameController.turn--;
+			GameController_1.user_touchable = false;
+			GameController_1.turn--;
 		}
 	}
 }
