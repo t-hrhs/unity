ea70c1f520b2ac2200fccc04b75fbfda02718a29
t-hrhs <hirohashi.t.0204@gmail.com>
2013-12-23 22:45:18 +0900

    【count_up】 : clear条件をだいたい実装

--- count_up/Assets/Script/Config.cs
+++ count_up/Assets/Script/Config.cs
@@ -6,17 +6,15 @@ using System;
 public static class  Config {
 	//stage_number-1 because of array index
 	public static int stage_id = 0;
+	public static bool clear_flag = false;
 	public static int[] ball_num = {4,4};
 
 	//index : stage_id
 	public static int[] max_turn = {3,4};
-
-	//TODO : too_complicated
-	/*public static Dictionary<int, Dictionary<string, double>>[] all_info = new Dictionary<int, Dictionary<string, double>>[]{
-		new Dictionary<int, Dictionary<string, double>> () {
-			{1, new Dictionary<string, double>() {{"x", 5.0f}}},
-		},
-	};*/
+	public static Dictionary<string, int>[] clear_cond = new Dictionary<string, int>[] {
+		new Dictionary<string, int>() {{"cyan",0}, {"yellow",0}, {"red",1}},
+		new Dictionary<string, int>() {{"cyan",0}, {"yellow",0}, {"red",2}},
+	};
 
 	//first_index : stage_id, second_index : ball_index
 	public static Dictionary<string, double>[][] ball_data = new Dictionary<string, double>[][]{
@@ -32,13 +30,8 @@ public static class  Config {
 			new Dictionary<string, double>() {{"x",1.0f},{"y",0.5f},{"z",0.0f},{"color",3.0f}},
 			new Dictionary<string, double>() {{"x",-5.0f},{"y",0.5f},{"z",0.0f},{"color",1.0f}},
 		},
-		//{
-			//new Dictionary<string, double>() {{"x",5.0f},{"y",0.5f},{"z",0.0f},{"color",1.0f}},
-			//new Dictionary<string, double>() {{"x",5.0f},{"y",0.5f},{"z",0.0f},{"color",1.0f}},
-			//new Dictionary<string, double>() {{"x",5.0f},{"y",0.5f},{"z",0.0f},{"color",1.0f}},
-		//},
 	}; 
-	//NOTE : DON'T USE THIS FUNCTION
+	//NOTE : DON'T USE THESE FUNCTION
 	/*
 	// Use this for initialization
 	void Start () {
